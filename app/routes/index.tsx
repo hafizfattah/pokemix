192cb128ac937c2c8bf870bd0b038514820d9057
hafiz <hafizxfattah@gmail.com>
2022-06-20 02:14:17 +0700

    infinite scroll

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,7 +5,7 @@ import PokemonList from "../components/PokemonList";
 
 import { getPokemons, getPokemonByName } from "~/api/pokemon";
 import styles from '../styles/home.css';
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 // SERVER SIDE
 export const loader: LoaderFunction = async ({request}) => {
@@ -30,26 +30,82 @@ export const meta: MetaFunction = () => ({
 
 //CLIENT SIDE
 export default function Index() {
-  let offset = 0;
   let isLoading = false;
   const response = useLoaderData();
+  const count = response?.count;
   const initialPokemon = response.data;
 
   const [pokemons, setPokemons] = useState(initialPokemon);
 
+  const [offset, setoffset] = useState(0);
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [clientHeight, setClientHeight] = useState(0);
+  const [pokemonListHeight, setPokemonListHeight] = useState(null);
+  const [shouldFetch, setShouldFetch] = useState(true);
+  const [isFetching, setIsFetching] = useState(false);
+
+
   const loadMore = async () => {
-    offset = offset + 20;
+    setoffset(offset + 20);
+    setIsFetching(true);
+
     const {data} = await getPokemons(20, offset)
     setPokemons((prevPokemons: any) => [...prevPokemons, ...data]);
+
+    setIsFetching(false);
+    if(pokemons.length >= count) {
+      setShouldFetch(false);
+      return
+    }
+    setShouldFetch(true);
   }
+
+  const divBottom = useCallback(
+    (node) => {
+      if (node !== null) {
+        setPokemonListHeight(node.getBoundingClientRect().height);
+      }
+    },
+    [pokemons.length]
+  );
+
+  useEffect(() => {
+    const scrollListener = () => {
+      setClientHeight(window.innerHeight);
+      setScrollPosition(window.scrollY);
+    };
+    // Avoid running during SSR
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", scrollListener);
+    }
+
+    // Clean up
+    return () => {
+      if (typeof window !== "undefined") {
+        window.removeEventListener("scroll", scrollListener);
+      }
+    };
+
+  }, []);
+
+  useEffect(() => {
+    if (!shouldFetch || !pokemonListHeight) return;
+    if (clientHeight + scrollPosition + 100 < pokemonListHeight) return;
+
+    loadMore()
+    setShouldFetch(false);
+
+  }, [clientHeight, scrollPosition]);
+
   
   return (
     <div>
       <SearchAndFilterForm />
-      <PokemonList pokemons={pokemons}/>
-
+      <div ref={divBottom}>
+        <PokemonList pokemons={pokemons}/>
+      </div>
       <div className="flex justify-center">
-        <button className="bubbly-button" onClick={loadMore}>{isLoading ? 'Loading' : 'Load More'}</button>
+        {isFetching ? 'Loading...' : 'No more pokemons'}
       </div> 
     </div>
   );
